106694e47b867446a8563562b91785f864d76e44
Gonzalo Tomatti <gonzalo98.g@hotmail.com>
2022-07-15 00:09:37 -0300

    asdadd

--- routes/signup.js
+++ routes/signup.js
@@ -6,9 +6,15 @@ router.route("/signup").post((req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
-  const newUser = new User({ username, password, email });
-  newUser.save();
-  res.json({ msg: "User created" });
+  User.find({ email: email }).then((foundEmail) => {
+    if (!foundEmail.length) {
+      const newUser = new User({ username, password, email });
+      newUser.save();
+      res.json({ msg: "User created" });
+    } else {
+      res.json({ msg: "email in use" });
+    }
+  });
 });
 
 module.exports = router;
